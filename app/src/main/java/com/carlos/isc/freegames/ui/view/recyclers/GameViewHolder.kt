3eebbbad343b690c1c97744ed2f3f78ec2957066
Carlos Antonio <carlos.antonio.isc@gmail.com>
2021-08-12 08:34:18 -0500

    [UPDATE] Added thumnail and toolbar

--- app/src/main/java/com/carlos/isc/freegames/ui/view/recyclers/GameViewHolder.kt
+++ app/src/main/java/com/carlos/isc/freegames/ui/view/recyclers/GameViewHolder.kt
@@ -1,6 +1,7 @@
 package com.carlos.isc.freegames.ui.view.recyclers
 
 import android.view.View
+import com.bumptech.glide.Glide
 import com.carlos.isc.freegames.databinding.ItemGameBinding
 import com.carlos.isc.freegames.domain.models.GameModel
 import com.carlos.isc.freegames.ui.view.base.BaseViewHolder
@@ -14,6 +15,7 @@ class GameViewHolder(view: View): BaseViewHolder<GameModel>(view) {
             ivGameTitle.text = item.title
             tvGameGenre.text = item.genre
             ivGameDescription.text = item.short_description
+            Glide.with(binding.root).load(item.thumbnail).centerCrop().into(binding.ivGameThumbnail)
         }
     }
 }
